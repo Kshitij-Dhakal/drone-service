e7eb448591ad79a597534c4cf7c13ef1ff65b000
Kshitij Dhakal <dhakalkshitij@gmail.com>
2023-07-10 21:28:21 +0545

    service test impl done

--- src/test/java/com/example/drones/dtomappers/DroneMapperTest.java
+++ src/test/java/com/example/drones/dtomappers/DroneMapperTest.java
@@ -1,20 +1,66 @@
 package com.example.drones.dtomappers;
 
+import com.example.drones.dtos.DroneDto;
+import com.example.drones.models.Drone;
+import com.example.drones.models.DroneModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 class DroneMapperTest {
+    @Autowired
+    private DroneMapper droneMapper;
 
     @Test
-    void droneDtoToDrone() {
-    }
+    void testDroneDtoToDrone() {
+        DroneDto droneDto = new DroneDto();
+        droneDto.setSerialNumber(UUID.randomUUID().toString());
+        DroneModel[] models = DroneModel.values();
+        droneDto.setDroneModel(models[ThreadLocalRandom.current().nextInt(models.length)]);
+        droneDto.setWeightLimit(ThreadLocalRandom.current().nextInt(500));
+        droneDto.setBatteryCapacity(ThreadLocalRandom.current().nextInt(0, 100));
 
-    @Test
-    void droneToDroneDto() {
+        Drone drone = droneMapper.droneDtoToDrone(droneDto);
+
+        assertEquals(droneDto.getSerialNumber(), drone.getSerialNumber());
+        assertEquals(droneDto.getDroneModel(), drone.getDroneModel());
+        assertEquals(droneDto.getWeightLimit(), drone.getWeightLimit());
+        assertEquals(droneDto.getBatteryCapacity(), drone.getBatteryCapacity());
     }
 
     @Test
     void testDroneToDroneDto() {
+        Drone drone = new Drone();
+        drone.setSerialNumber(UUID.randomUUID().toString());
+        DroneModel[] models = DroneModel.values();
+        drone.setDroneModel(models[ThreadLocalRandom.current().nextInt(models.length)]);
+        drone.setWeightLimit(ThreadLocalRandom.current().nextInt(500));
+        drone.setBatteryCapacity(ThreadLocalRandom.current().nextInt(0, 100));
+
+        DroneDto droneDto = droneMapper.droneToDroneDto(drone);
+
+        assertEquals(drone.getSerialNumber(), droneDto.getSerialNumber());
+        assertEquals(drone.getDroneModel(), droneDto.getDroneModel());
+        assertEquals(drone.getWeightLimit(), droneDto.getWeightLimit());
+        assertEquals(drone.getBatteryCapacity(), droneDto.getBatteryCapacity());
+    }
+
+    @Test
+    void testDroneToDroneDtoList() {
+        List<Drone> drones = new ArrayList<>();
+        drones.add(new Drone());
+        drones.add(new Drone());
+
+        List<DroneDto> droneDtos = droneMapper.droneToDroneDto(drones);
+
+        assertEquals(2, droneDtos.size());
     }
 }
--- src/test/java/com/example/drones/models/DroneStateTest.java
+++ src/test/java/com/example/drones/models/DroneStateTest.java
@@ -7,10 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class DroneStateTest {
 
     @Test
-    void forName() {
+    void testForName() {
+        assertEquals(DroneState.DRONE_STATE_IDLE, DroneState.forName("DRONE_STATE_IDLE"));
+        assertEquals(DroneState.DRONE_STATE_UNKNOWN, DroneState.forName("DRONE_STATE_UNKNOWN"));
     }
 
     @Test
-    void forNumber() {
+    void testForNumber() {
+        assertEquals(DroneState.DRONE_STATE_IDLE, DroneState.forNumber(1));
+        assertEquals(DroneState.DRONE_STATE_UNKNOWN, DroneState.forNumber(999));
+    }
+
+    @Test
+    void testNumber() {
+        assertEquals(1, DroneState.DRONE_STATE_IDLE.getNumber());
+        assertEquals(0, DroneState.DRONE_STATE_UNKNOWN.getNumber());
+    }
+
+    @Test
+    void testName() {
+        assertEquals("DRONE_STATE_IDLE", DroneState.DRONE_STATE_IDLE.getName());
+        assertEquals("DRONE_STATE_UNKNOWN", DroneState.DRONE_STATE_UNKNOWN.getName());
     }
 }
