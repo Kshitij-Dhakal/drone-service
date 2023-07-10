44b29132ec79e151cec4e98ac5324325347ce757
Kshitij-Dhakal <TW*PHwDat2nvM*c5Y^pQ#ZhrurcUXjr3GzPQ#3ezfFwXE3J5ANwyK&SbaYMX>
2023-07-10 08:35:54 +0545

    Test initial setup done

--- src/test/java/com/example/drones/DroneApplicationTests.java
+++ src/test/java/com/example/drones/DroneApplicationTests.java
@@ -9,5 +9,4 @@ class DroneApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
--- src/test/java/com/example/drones/dtomappers/DroneLogMapperTest.java
+++ src/test/java/com/example/drones/dtomappers/DroneLogMapperTest.java
@@ -1,13 +1,25 @@
 package com.example.drones.dtomappers;
 
+import com.example.drones.models.Drone;
+import com.example.drones.models.DroneLog;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.ThreadLocalRandom;
 
+@SpringBootTest
 class DroneLogMapperTest {
+    @Autowired
+    private DroneLogMapper droneLogMapper;
 
     @Test
     void droneLogToDroneLogDto() {
+        DroneLog droneLog = new DroneLog();
+        Drone drone = new Drone();
+        droneLog.setId(ThreadLocalRandom.current().nextLong());
+        droneLog.setDrone(drone);
+        droneLogMapper.droneLogToDroneLogDto(droneLog);
     }
 
     @Test
